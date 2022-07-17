e3f0272a07c9575a2e18b0285962d92ed550cbf3
Vlad Shevchenko <busterskh@gmail.com>
2022-07-17 23:24:42 +0300

    Done tasks

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
-
+import random
 from django.views import View
+
+elements = ['Cat', 'Car', 'House', 'Human', 'Dog', 'Horse', 'Deer', 'Forest', 'Bird', '10']
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
+        global elements
         return HttpResponse('<ul>'
-                            '<li>Установить python - сделано!</li>'
-                            '<li>Установить django- сделано!</li>'
-                            '<li>Запустить сервер- сделано!</li>'
-                            '<li>Порадоваться результату- сделано!</li>'
+                            f'<li>{random.choice(elements)}</li>'
+                            f'<li>{random.choice(elements)}</li>'
+                            f'<li>{random.choice(elements)}</li>'
+                            f'<li>{random.choice(elements)}</li>'
+                            f'<li>{random.choice(elements)}</li>'
                             '</ul>')
